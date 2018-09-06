1524a8b0b90a9e7df075c9274d6fec37fe900475
Sjors <sjorsottjes@gmail.com>
2018-09-06 13:04:59 +0200

    add assertions to the RequestHistory class

--- src/Support/RequestHistory.php
+++ src/Support/RequestHistory.php
@@ -5,6 +5,8 @@ namespace SjorsO\Gobble\Support;
 use ArrayAccess;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
+use Illuminate\Support\Arr;
+use PHPUnit\Framework\Assert as PHPUnit;
 use RuntimeException;
 
 class RequestHistory implements ArrayAccess
@@ -28,6 +30,56 @@ class RequestHistory implements ArrayAccess
         $this->error = $guzzleHistory['error'];
 
         $this->options = $guzzleHistory['options'];
+    }
+
+    public function requestBody()
+    {
+        return $this->request->getBody()->getContents();
+    }
+
+    public function decodeRequestJson($key = null)
+    {
+        $decodedResponse = json_decode($this->requestBody(), true);
+
+        if (is_null($decodedResponse) || $decodedResponse === false) {
+            PHPUnit::fail('Request contains invalid JSON.');
+        }
+
+        return data_get($decodedResponse, $key);
+    }
+
+    public function assertRequestBodyExact($expected)
+    {
+        PHPUnit::assertSame($expected, $this->requestBody());
+
+        return $this;
+    }
+
+    public function assertRequestBodyJson(array $data, $strict = false)
+    {
+        PHPUnit::assertArraySubset($data, $this->decodeRequestJson(), $strict);
+
+        return $this;
+    }
+
+    public function assertRequestBodyExactJson(array $expected)
+    {
+        $expected = json_encode(Arr::sortRecursive($expected));
+
+        $actual = json_encode(Arr::sortRecursive(
+            (array) $this->decodeRequestJson()
+        ));
+
+        PHPUnit::assertSame($expected, $actual);
+
+        return $this;
+    }
+
+    public function assertRequestUri($expected)
+    {
+        PHPUnit::assertSame($expected, (string) $this->request->getUri());
+
+        return $this;
     }
 
     public function offsetExists($offset)
--- tests/Unit/Support/RequestHistoryTest.php
+++ tests/Unit/Support/RequestHistoryTest.php
@@ -2,6 +2,7 @@
 
 namespace SjorsO\Gobble\Tests\Unit\Support;
 
+use SjorsO\Gobble\Facades\Gobble;
 use SjorsO\Gobble\GuzzleFakeWrapper;
 use SjorsO\Gobble\Support\RequestHistory;
 use SjorsO\Gobble\Tests\TestCase;
@@ -26,5 +27,61 @@ class RequestHistoryTest extends TestCase
         $this->assertSame($history->response, $history['response']);
         $this->assertSame($history->error,    $history['error']);
         $this->assertSame($history->options,  $history['options']);
+    }
+
+    /** @test */
+    function it_can_assert_the_request_uri()
+    {
+        Gobble::fake()->pushEmptyResponse();
+
+        Gobble::get('https://laravel.com');
+
+        Gobble::lastRequest()->assertRequestUri('https://laravel.com');
+    }
+
+    /** @test */
+    function it_can_assert_the_exact_request_body()
+    {
+        Gobble::fake()->pushEmptyResponse();
+
+        Gobble::post('https://laravel.com', ['body' => 'Foo']);
+
+        Gobble::lastRequest()->assertRequestBodyExact('Foo');
+    }
+
+    /** @test */
+    function it_can_assert_the_request_body_contains_a_subset_of_json()
+    {
+        Gobble::fake()->pushEmptyResponse();
+
+        Gobble::post('https://laravel.com', [
+            'json' => [
+                'deviceIds' => ['aa1', 'aa2'],
+                'magicWord' => 'Please',
+            ],
+        ]);
+
+        Gobble::lastRequest()->assertRequestBodyJson([
+            'deviceIds' => ['aa1'],
+            'magicWord' => 'Please',
+        ]);
+    }
+
+    /** @test */
+    function it_can_assert_the_exact_request_json_body()
+    {
+        Gobble::fake()->pushEmptyResponse();
+
+        Gobble::post('https://laravel.com', [
+            'json' => [
+                'deviceIds' => ['aa1', 'aa2'],
+                'magicWord' => 'Please',
+            ],
+        ]);
+
+        Gobble::lastRequest()->assertRequestBodyExactJson([
+            'magicWord' => 'Please',
+            'deviceIds' => ['aa1', 'aa2'],
+        ]);
     }
 }
